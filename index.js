7e6ee9439a2e6675b66e05992449c12c5fe9901b
Vergel Mañalac <verge2015@gmail.com>
2022-08-17 18:42:41 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { AvatarGenerator } = require("random-avatar-generator");
+const fs = require("fs");
+const https = require("https");
+
+const generator = new AvatarGenerator();
+const avatarUrl = generator.generateRandomAvatar();
+
+https.get(avatarUrl, (res) => {
+    const path = "./avatar.svg";
+    const fileStream = fs.createWriteStream(path);
+    res.pipe(fileStream);
+    
+    fileStream.on("finish", () => {
+        fileStream.close();
+        console.log("File written");
+    });
+});
+
